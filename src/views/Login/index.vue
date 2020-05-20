b6dde089b682101205e5229843d7d4c0d95a3f78
ZY <13041895753@163.com>
2020-05-20 13:18:03 +0800

    ''

---
+++ src/views/Login/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="login"  >
+     <div class="login-wrap">
+       <ul class="menu-tab">
+         <li v-for="item in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
+       </ul>
+       <el-form :model="ruleForm" class="login-form" size="medium">
+
+         <el-form-item  class="item-form">
+           <label  > 邮箱</label>
+           <el-input   v-model="ruleForm.username" autocomplete="off"></el-input>
+         </el-form-item>
+
+         <el-form-item  class="item-form">
+           <label  > 密码</label>
+           <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+         </el-form-item>
+
+         <el-form-item   class="item-form">
+           <label  > 验证码</label>
+           <el-row :gutter="10">
+             <el-col :span="15">
+                <el-input type="password" v-model="ruleForm.code" autocomplete="off"></el-input></el-col>
+             <el-col :span="9">
+                <el-button type="success" @click="submitForm('ruleForm')" class=" block">获取验证码</el-button>
+             </el-col>
+           </el-row>
+         </el-form-item>
+
+         <el-form-item   class="item-form">
+           <label  > 验证码</label>
+           <el-input v-model.number="ruleForm.age"></el-input>
+         </el-form-item>
+
+         <el-form-item>
+           <el-button type="primary" @click="submitForm('ruleForm')" class="login-btn block">提交</el-button>
+         </el-form-item>
+
+       </el-form>
+     </div>
+  </div>
+</template>
+
+<script >
+
+export default {
+    name:'login',
+    data(){
+
+      return{
+        menuTab:[
+          {txt:'登录',current:true},
+          {txt:'注册',current:false}
+        ],
+        ruleForm: {
+          username: '',
+          password: '',
+          code: ''
+        },
+      };
+    },
+    methods:{
+      toggleMenu(data){
+        this.menuTab.forEach(elem=>{
+          elem.current=false
+        })
+        data.current=true
+      },
+      submitForm(formName) {
+
+      },
+
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+  #login{
+    height: 100vh;
+    background-color: #344a5f;
+  }
+  .login-wrap{
+    width: 330px;
+    margin: auto;
+  }
+  .menu-tab{
+    text-align: center;
+    li{
+      display: inline-block;
+      width: 88px;
+      line-height: 36px;
+      font-size: 14px;
+      color: #fff;
+      border-radius: 2px;
+      cursor: pointer;
+    }
+    .current{
+      background-color: rgba(0,0,0,.1);
+    }
+  }
+  .login-form{
+     label{
+       display: inline-block;
+       margin-bottom: 3px;
+       font-size: 14px;
+       color: #fff;
+     }
+     .item-form{
+       margin-bottom: 13px;
+     }
+     .block{
+       display: block;
+       width: 100%;
+     }
+     .login-btn{
+
+     }
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+
+module.exports = {
+  publicPath: './', // 基本路径
+  outputDir: 'dist', // 输出文件目录
+  lintOnSave: false, // eslint-loader 是否在保存的时候检查
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  // webpack配置
+  chainWebpack: (config) => {
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+      config.mode = 'production'
+    } else {
+      // 为开发环境修改配置...
+      config.mode = 'development'
+    }
+    Object.assign(config, {
+      // 开发生产共同配置
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        } // 别名配置
+      }
+    })
+  },
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // css相关配置
+  css: {
+    extract: true, // 是否使用css分离插件 ExtractTextPlugin
+    sourceMap: false, // 开启 CSS source maps?
+    loaderOptions: {
+      sass:{
+        prependData:`@import "./src/styles/main.scss";`
+      }
+    },
+    // css预设器配置项 详见https://cli.vuejs.org/zh/config/#css-loaderoptions
+    modules: false // 启用 CSS modules for all css / pre-processor files.
+  },
+
+  // 是否为 Babel 或 TypeScript 使用 thread-loader。
+  //该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
+  parallel: require('os').cpus().length > 1,
+  // PWA 插件相关配置 see
+  //https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+  pwa: {},
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0', // 允许外部ip访问
+    port: 8022, // 端口
+    https: false, // 启用https
+    overlay: {
+      warnings: true,
+      errors: true
+    }, // 错误、警告在页面弹出
+  },
+  // 第三方插件配置
+  pluginOptions: {}
+}
